68f28dd7da017b935658e9fd4ac8a768aaaa4710
alexandrefnsc <alexandre.fnsc@gmail.com>
2014-11-27 14:09:44 -0200

    correção do models para melhorar o recorte das fotos

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
 	validates :title, :body, :brand, :price, :buyit, :category, :image, presence: true
 	belongs_to :brand
 	belongs_to :category
-	uploads_one :image, thumb: [90, 80], small: [340, 240], regular: [580, 240]
+	uploads_one :image, thumb: [90, 80], small: [340, 240], regular: [380, 380]
 
 	def to_param
 		"#{id} #{title}".parameterize
